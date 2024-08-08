57d5bdba8bfec7a2b841514e33c7d8d719c16fc6
Jeff Nyman <jeffnyman@gmail.com>
2024-08-08 14:53:16 -0500

    Update hooks.

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,10 +1,9 @@
-import { BeforeAll, AfterAll, Before, After } from "@cucumber/cucumber";
-import { chromium, Page, Browser, BrowserContext } from "@playwright/test";
+import { BeforeAll, AfterAll, Before, After, Status } from "@cucumber/cucumber";
+import { chromium, Browser, BrowserContext } from "@playwright/test";
 import { pageFixture } from "./pageFixture";
 
 let browser: Browser;
 let context: BrowserContext;
-let page: Page;
 
 BeforeAll(async () => {
   browser = await chromium.launch({ headless: false });
@@ -12,17 +11,19 @@ BeforeAll(async () => {
 
 Before(async () => {
   context = await browser.newContext();
-  page = await context.newPage();
+  const page = await context.newPage();
   pageFixture.page = page;
 });
 
-After(async function ({ pickle }) {
-  const img = await pageFixture.page.screenshot({
-    path: `./test-result/screenshots/${pickle.name}.png`,
-    type: "png",
-  });
+After(async function ({ pickle, result }) {
+  if (result?.status == Status.FAILED) {
+    const img = await pageFixture.page.screenshot({
+      path: `./test-result/screenshots/${pickle.name}.png`,
+      type: "png",
+    });
 
-  await this.attach(img, "image/png");
+    await this.attach(img, "image/png");
+  }
 
   await pageFixture.page.close();
   await context.close();
