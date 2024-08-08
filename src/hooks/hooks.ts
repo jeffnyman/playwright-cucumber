7fe6ef1af511c3ce3943e2f404fc2053778d59ef
Jeff Nyman <jeffnyman@gmail.com>
2024-08-08 14:43:30 -0500

    Add screenshot to hooks.

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,19 +1,33 @@
-import { BeforeAll, AfterAll } from "@cucumber/cucumber";
-import { chromium, Page, Browser } from "@playwright/test";
+import { BeforeAll, AfterAll, Before, After } from "@cucumber/cucumber";
+import { chromium, Page, Browser, BrowserContext } from "@playwright/test";
 import { pageFixture } from "./pageFixture";
 
 let browser: Browser;
+let context: BrowserContext;
 let page: Page;
-
-// BeforeAll(async function () {});
 
 BeforeAll(async () => {
   browser = await chromium.launch({ headless: false });
-  page = await browser.newPage();
+});
+
+Before(async () => {
+  context = await browser.newContext();
+  page = await context.newPage();
   pageFixture.page = page;
 });
 
-AfterAll(async () => {
+After(async function ({ pickle }) {
+  const img = await pageFixture.page.screenshot({
+    path: `./test-result/screenshots/${pickle.name}.png`,
+    type: "png",
+  });
+
+  await this.attach(img, "image/png");
+
   await pageFixture.page.close();
+  await context.close();
+});
+
+AfterAll(async () => {
   await browser.close();
 });
